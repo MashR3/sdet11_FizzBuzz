3efaa62f3dc1f5ec39cf9bca3ac6a89257b3a9b3
MashR3 <michaelash17@googlemail.com>
2017-10-17 21:17:19 +0100

    More sensible to me?

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,26 +1,11 @@
-class Fizzbuzz(num)
-
-  puts "FizzBuzz" if CheckFizzBuzz(num)
-  puts "Buzz" if CheckFizz(num)
-  puts "Fizz" if CheckBuzz(num)
-  num
-
-  def CheckFizz(num)
-    num % 3 == 0
+class Fizzbuzz
+  def check(num)
+    if num % 15 == 0
+      'fizzbuzz'
+    elsif num % 3 == 0
+      'fizz'
+    else num % 5 == 0
+      'buzz'
+    end
   end
-
-  def CheckBuzz(num)
-    num % 5 == 0
-  end
-
-  def CheckFizzBuzz(num)
-    num % 15 == 0
-  end
-
 end
-
-# (1..100).map { |num| puts FizzBuzz(num) }
-
-# def range(num1, num2) --DEFFO INCLUDE this somewhere
-  # num1..num2
-# end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,50 +1,25 @@
 require_relative 'spec_helper'
 
-describe Fizzbuzz do
+describe 'Fizzbuzz cal' do
+  before do
+    @fizzbuzz = Fizzbuzz.new
+  end
+
   context 'should know what the numbers are divisible by' do
     it '3' do
-      expect CheckFizz(3) == true
+      expect @fizzbuzz.check(3) == true
     end
 
     it '5' do
-      expect CheckBuzz(5) == true
+      expect @fizzbuzz.check(5) == true
     end
 
     it '15' do
-      expect CheckFizzBuzz(15) == true
+      expect @fizzbuzz.check(15) == true
     end
   end
-
-  context 'should know what the numbers are not divisible by' do
-    it '3' do
-      expect CheckFizz(1) == false
-    end
-
-    it '5' do
-      expect CheckBuzz(1) == false
-    end
-
-    it '3' && '5' do
-      expect CheckFizzBuzz(1) == false
-    end
-  end
-
-  context 'should return' do
-    it 'the number' do
-      expect Fizzbuzz(1) == 1
-    end
-    it 'fizz' do
-      expect Fizzbuzz(3) == "Fizz"
-    end
-    it 'buzz' do
-      expect Fizzbuzz(5) == "Buzz"
-    end
-    it 'fizzbuzz' do
-      expect Fizzbuzz(15) == "FizzBuzz"
-    end
-  end
-
 end
+
 
 # Within an object
 # Instantiated
